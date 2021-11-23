ab431c0ade5787cc8ea3cbbe10db88a2e6f425ab
JustenNicanor <92636904+JustenNicanor@users.noreply.github.com>
2021-11-23 14:09:57 +0800

    Second commit(Third and Fourth number as the highest)

--- Assignment 6(1).py
+++ Assignment 6(1).py
@@ -1,6 +1,3 @@
-#Create a program that ask 4 numbers. 
-#Print the 4 numbers from highest to lowest using only if-else statement
-
 def Four_Numbers():
     Firstnum = int(input("Enter First number:"))
     Secondnum = int(input("Enter Second number:"))
@@ -30,16 +27,34 @@ def Four_Numbers():
         print (f"Highest to lowest of numbers: {Secondnum},{Fourthnum},{Firstnum},{Thirdnum}")
     elif Secondnum > Fourthnum and Fourthnum > Thirdnum and Thirdnum > Firstnum:
         print (f"Highest to lowest of numbers: {Secondnum},{Fourthnum},{Thirdnum},{Firstnum}")
-
-    
+    elif Thirdnum > Firstnum and Firstnum > Secondnum and Secondnum > Fourthnum:
+        print (f"Highest to lowest of numbers: {Thirdnum},{Firstnum},{Secondnum},{Fourthnum}")
+    elif Thirdnum > Firstnum and Firstnum > Fourthnum and Fourthnum > Secondnum:
+        print (f"Highest to lowest of numbers: {Thirdnum},{Firstnum},{Fourthnum},{Secondnum}")
+    elif Thirdnum > Secondnum and Secondnum > Firstnum and Firstnum > Fourthnum:
+        print (f"Highest to lowest of numbers: {Thirdnum},{Secondnum},{Firstnum},{Fourthnum}")
+    elif Thirdnum > Secondnum and Secondnum > Fourthnum and Fourthnum > Firstnum:
+        print (f"Highest to lowest of numbers: {Thirdnum},{Secondnum},{Fourthnum},{Firstnum}")
+    elif Thirdnum > Fourthnum and Fourthnum > Firstnum and Firstnum > Secondnum:
+        print (f"Highest to lowest of numbers: {Thirdnum},{Fourthnum},{Firstnum},{Secondnum}")
+    elif Thirdnum > Fourthnum and Fourthnum > Secondnum and Secondnum > Firstnum:
+        print (f"Highest to lowest of numbers: {Thirdnum},{Fourthnum},{Secondnum},{Firstnum}")
+    elif Fourthnum > Firstnum and Firstnum > Secondnum and Secondnum > Thirdnum:
+        print (f"Highest to lowest of numbers: {Fourthnum},{Firstnum},{Secondnum},{Thirdnum}")
+    elif Fourthnum > Firstnum and Firstnum > Thirdnum and Thirdnum > Secondnum:
+        print (f"Highest to lowest of numbers: {Fourthnum},{Firstnum},{Thirdnum},{Secondnum}")
+    elif Fourthnum > Secondnum and Secondnum > Firstnum and Firstnum > Thirdnum:
+        print (f"Highest to lowest of numbers: {Fourthnum},{Secondnum},{Firstnum},{Thirdnum}")
+    elif Fourthnum > Secondnum and Secondnum > Thirdnum and Thirdnum > Firstnum:
+        print (f"Highest to lowest of numbers: {Fourthnum},{Secondnum},{Thirdnum},{Firstnum}")
+    elif Fourthnum > Thirdnum and Thirdnum > Firstnum and Firstnum > Secondnum:
+        print (f"Highest to lowest of numbers: {Fourthnum},{Thirdnum},{Firstnum},{Secondnum}")
+    elif Fourthnum > Thirdnum and Thirdnum > Secondnum and Secondnum > Firstnum:
+        print (f"Highest to lowest of numbers: {Fourthnum},{Thirdnum},{Secondnum},{Firstnum}")
+    else:
+        print ("The numbers you input has the same number")
         
 
-
-
-
-
-
-
-
-
 num = Four_Numbers()
+
+
