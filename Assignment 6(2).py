c6766aba76dd98f2338a99f6771d71149bab7db1
JustenNicanor <92636904+JustenNicanor@users.noreply.github.com>
2021-11-24 02:36:13 +0800

    Second commit(Adding,adjusting and changing in each line to add the total score of user)

--- Assignment 6(2).py
+++ Assignment 6(2).py
@@ -1,18 +1,21 @@
 def Quiz():
     import random
+    Score = 0
     for i in range (10):
         num1 = random.randint(0,99)
         num2 = random.randint(0,99)
         ans = num1+num2
-        print(f" what is {num1} + {num2}?")
-        plrans = int(input("Answer: ")) 
-        if plrans == ans : 
-            Score = Score + 1
+        print(f" What is {num1} + {num2}?")
+        useranswer = int(input("Answer: "))
+        if useranswer == ans: 
+            Score += 1
             print("CORRECT!")
-        else: 
-            print(f"INCORRECT the answer is {ans}")
-    print(f"Your score is,{Score}/10 ")
+        else:
+            print(f"INCORRECT, the answer is {ans}")
+    print (f"Your score is {Score}/10")
+
+
+quiz_= Quiz()
 
 
 
-quiz_ = Quiz()
